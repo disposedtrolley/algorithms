0fdf78f29400fe208165100924409c6ffa2c3156
James Liu <james_liu@fastmail.com>
2019-08-31 13:37:41 +1000

    feat: cleanup

--- recursion/permute.rkt
+++ recursion/permute.rkt
@@ -12,16 +12,19 @@
      ;; there are letters to be chosen in s, so for each letter, we need to choose it,
      ;; explore the resulting path, and then unchoose it
      (for ([char s])
-       ;;(displayln (format "char: ~a chosen: ~a" char chosen))
+
+       ;; choose
        (define char-idx (index-of s char))
        (set! chosen (append chosen (list char)))
+       (displayln (format "s: ~a char: ~a chosen: ~a" s char chosen))
        (set! s (remove char s))
+
+       ;; explore
        (permute-helper s chosen)
 
        ;; un-choose
        (set! s (insert-between s char char-idx))
-       (set! chosen (take chosen (- (length chosen) 1)))
-       )
+       (set! chosen (take chosen (- (length chosen) 1))))
      ]))
 
 (define (permute s)
